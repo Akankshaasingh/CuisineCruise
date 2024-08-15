783071a79003e3331b31d920d6a6376d3bd4a510
Akanksha <akankshasinghsindhu@gmail.com>
2024-08-15 11:31:41 +0530

    Episode 6 : CusineCruise Running with the search function

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,33 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 
-
 const Header = () => {
-    return(
-        <div className="header">
-            <div className="logo-container">
-                <img className="logo" src={LOGO_URL}></img>
-            </div>
-            <div className="nav-items">
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                    <li>Cart</li>
-                </ul>
-            </div>
+  const [btnNameReact, setBtnNameReact] = useState("Login");
 
-        </div>
-    );
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={LOGO_URL}></img>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+          <button
+            className="login"
+            onClick={() => {
+              btnNameReact === "Login"
+                ? setBtnNameReact("Logout")
+                : setBtnNameReact("Login");
+            }}
+          >
+            {btnNameReact}
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
 };
 export default Header;
